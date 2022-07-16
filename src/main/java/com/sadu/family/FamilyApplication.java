a9aab8e39f66c864b5b14585ed2a18f902341738
sadvik-gowda <sadvikb.d@hcl.com>
2022-07-16 17:21:04 +0530

    Added logs

--- src/main/java/com/sadu/family/FamilyApplication.java
+++ src/main/java/com/sadu/family/FamilyApplication.java
@@ -1,12 +1,16 @@
 package com.sadu.family;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FamilyApplication {
 
 	public static void main(String[] args) {
+		log.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7777");
+		log.info("FamilyApplication is started");
 		SpringApplication.run(FamilyApplication.class, args);
 	}
 
--- src/main/java/com/sadu/family/controller/PersonController.java
+++ src/main/java/com/sadu/family/controller/PersonController.java
@@ -2,6 +2,7 @@ package com.sadu.family.controller;
 
 import com.sadu.family.dto.PersonDataDto;
 import com.sadu.family.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +11,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PersonController {
 
     @Autowired
     PersonService personService;
 
+    @GetMapping("/")
+    public String msg(){
+        log.info("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7777");
+        log.info("msg method is called");
+        return "Welcome to app";
+    }
+
     @GetMapping("/get-data/{name}")
     public ResponseEntity<PersonDataDto> getPersonByName(@PathVariable("name") String name) {
+        log.info("getPersonByName method is called");
+
         PersonDataDto responseData = personService.getPersonByName(name);
         return new ResponseEntity<>(responseData, HttpStatus.OK);
 
